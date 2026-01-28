40776ce2f66e0e124e7f527fe158323d4dc973f8
Blohhs <Domikblohi@gmail.com>
2026-01-28 12:37:38 +0300

    pepe

--- src/mappers.js
+++ src/mappers.js
@@ -1,1 +1,12 @@
+export const mapToNames = (products) => {
+  return products.map(product => product.name);
+};
 
+export const mapToPriceWithTax = (taxRate) => {
+  return (products) => {
+    return products.map(product => ({
+      ...product,
+      price: Number((product.price * (1 + taxRate)).toFixed(2))
+    }));
+  };
+};
