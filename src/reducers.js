d46562533069929ac517320759d1e38b68b99545
Blohhs <Domikblohi@gmail.com>
2026-01-28 12:36:36 +0300

    pepe

--- src/reducers.js
+++ src/reducers.js
@@ -1,1 +1,38 @@
+export const sumPrices = (products) => {
+  return products.reduce((total, product) => total + product.price, 0);
+};
 
+
+export const groupByCategory = (products) => {
+  return products.reduce((grouped, product) => {
+    const category = product.category;
+    
+    if (!grouped[category]) {
+      grouped[category] = [];
+    }
+    
+    grouped[category].push(product);
+    return grouped;
+  }, {});
+};
+
+
+export const countByCategory = (products) => {
+  return products.reduce((counts, product) => {
+    const category = product.category;
+    counts[category] = (counts[category] || 0) + 1;
+    return counts;
+  }, {});
+};
+
+
+export const averagePriceByCategory = (products) => {
+  const grouped = groupByCategory(products);
+  
+  return Object.keys(grouped).reduce((averages, category) => {
+    const categoryProducts = grouped[category];
+    const total = categoryProducts.reduce((sum, product) => sum + product.price, 0);
+    averages[category] = Number((total / categoryProducts.length).toFixed(2));
+    return averages;
+  }, {});
+};
