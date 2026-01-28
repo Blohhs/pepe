c5b495980d03833bddc655c3d8b811b4fe201df9
Blohhs <Domikblohi@gmail.com>
2026-01-28 12:38:11 +0300

    pepe

--- src/filters.js
+++ src/filters.js
@@ -1,1 +1,9 @@
+export const filterByCategory = (category) => {
+  return (products) => {
+    return products.filter(product => product.category === category);
+  };
+};
 
+export const filterInStock = (products) => {
+  return products.filter(product => product.inStock === true);
+};
